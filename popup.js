6759546cd98949222cb66326df162dd712aaabb4
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-29 22:46:31 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 chrome.tabs.query({ active: !0, currentWindow: !0 }, tabs => {
   let { origin } = new URL(tabs[0].url);
   let nodes = document.body.childNodes;
-  let checks = localStorage.getItem(origin);
   let i = 0;
+  let checks = localStorage.getItem(origin);
   if (checks)
     while (
       nodes[i].firstChild.checked = +checks[i],
       ++i < 7
     );
   b.onclick = () => {
-    checks = [0,0,0,0,0,0,0];
+    let checks = [0,0,0,0,0,0,0];
     let i = 0;
     while (
       checks[i] = nodes[i].firstChild.checked,
